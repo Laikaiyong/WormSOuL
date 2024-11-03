6e4180eee580bbf07f682a99dbd287d34b066eb8
Vandyck <vandycklai@gmail.com>
2024-11-03 16:34:15 +0800

    Upload latest

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # WormSOuL
 
+Memecoin Management makes easy with memecoin over crosschain
+
 ## Pitch Deck
-https://www.canva.com/design/DAGVZ6mRUtU/pGMuWNW8YCanjY-e8rhQwA/edit?utm_content=DAGVZ6mRUtU&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton
+https://www.canva.com/design/DAGVZ6mRUtU/pGMuWNW8YCanjY-e8rhQwA/edit
 
 ## Pitch Video
 
-
 ## Deployed Link
+https://worm-soul.vercel.app/
+
---
+++ src/utils/wormhole.ts
@@ -0,0 +1,20 @@
+import { wormhole } from "@wormhole-foundation/sdk";
+import algorand from "@wormhole-foundation/sdk/algorand";
+import aptos from "@wormhole-foundation/sdk/aptos";
+import cosmwasm from "@wormhole-foundation/sdk/cosmwasm";
+import evm from "@wormhole-foundation/sdk/evm";
+import solana from "@wormhole-foundation/sdk/solana";
+import sui from "@wormhole-foundation/sdk/sui";
+
+export const initWormhole = async () => {
+  const wh = await wormhole("Testnet", [
+    evm,
+    solana,
+    aptos,
+    algorand,
+    cosmwasm,
+    sui,
+  ]);
+
+  return wh;
+};
